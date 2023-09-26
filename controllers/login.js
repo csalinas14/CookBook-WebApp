73fdb2bf7fb31fc4bde75bae3961bbb3559fc812
Carlos Salinas <salinas.carlos14@gmail.com>
2023-09-26 17:33:03 -0400

    Updated login controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,36 +1,39 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const loginRouter = require("express").Router();
-const User = require("../models/user");
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
 
-loginRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
 
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username })
 
   //console.log(user);
 
   const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: "Invalid username or password",
-    });
+      error: 'Invalid username or password',
+    })
   }
 
   const userForToken = {
     username: user.username,
     id: user._id,
-  };
+  }
   //console.log(userForToken);
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  const token = jwt.sign(userForToken, process.env.SECRET)
   //console.log(token);
+  await user.populate('recipes')
+  response.status(200).send({
+    token,
+    username: user.username,
+    name: user.name,
+    recipes: user.recipes,
+  })
+})
 
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name });
-});
-
-module.exports = loginRouter;
+module.exports = loginRouter
