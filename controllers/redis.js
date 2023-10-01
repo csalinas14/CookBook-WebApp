2e69ba3b5268c1fd4c9cb399da100ac85c366116
Carlos Salinas <salinas.carlos14@gmail.com>
2023-10-01 15:46:05 -0400

    Updating redis config

--- controllers/redis.js
+++ controllers/redis.js
@@ -1,12 +1,16 @@
 const config = require('../utils/config')
 const logger = require('../utils/logger')
 const { createClient } = require('redis')
-const redisClient = createClient(config.REDIS_PORT)
+const redisClient = createClient({
+  url: config.REDIS_URL,
+  port: config.REDIS_PORT,
+})
 
 redisClient
   .connect()
   .then(() => {
     logger.info('connected to Redis')
+    logger.info(config.REDIS_URL)
   })
   .catch((error) => {
     logger.error('error connecting to Redis:', error.message)
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const REDIS_PORT = process.env.REDIS_PORT
+const PORT = process.env.PORT || 3003
+const REDIS_URL =
+  process.env.NODE_ENV === 'production' ? process.env.REDIS_URL : ''
+const REDIS_PORT = process.env.REDIS_PORT || 6379
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
@@ -10,13 +12,14 @@ const MONGODB_URI =
 const API_KEY = process.env.API_KEY
 
 const API_BASEURL = 'https://api.spoonacular.com/recipes/'
-const RECIPES_PER_PAGE = 2
+const RECIPES_PER_PAGE = 8
 
 module.exports = {
   MONGODB_URI,
   PORT,
   API_KEY,
   API_BASEURL,
+  REDIS_URL,
   REDIS_PORT,
   RECIPES_PER_PAGE,
 }
