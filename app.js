fe8fa8ce1908f28a46cadc9832b87d3066510a36
Carlos Salinas <salinas.carlos14@gmail.com>
2023-10-01 17:19:02 -0400

    Adding build

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const express = require("express");
-require("express-async-errors");
-const app = express();
-const cors = require("cors");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
-const config = require("./utils/config");
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-const recipesRouter = require("./controllers/recipes");
-const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
-const middleware = require("./utils/middleware");
-mongoose.set("strictQuery", false);
+const recipesRouter = require('./controllers/recipes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
-logger.info("connecting to", config.MONGODB_URI);
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info("connected to MongoDB");
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
-  });
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-app.use(middleware.tokenExtractor);
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
-app.use("/api/recipes", recipesRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
+app.use('/api/recipes', recipesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
